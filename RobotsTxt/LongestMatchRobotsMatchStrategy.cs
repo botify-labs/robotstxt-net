0f5febc093b620507b1538357c8e6eab72d019ec
Yves Bastide <stid@acm.org>
2023-06-28 21:30:22 +0200

    Initial commit: LongestMatchRobotsMatchStrategy

---
+++ RobotsTxt/LongestMatchRobotsMatchStrategy.cs
@@ -0,0 +1,71 @@
+﻿namespace RobotsTxt
+{
+    /// <summary>
+    /// A RobotsMatchStrategy defines a strategy for matching individual lines in a
+    /// robots.txt file. Each Match* method should return a match priority, which is
+    /// interpreted as:
+    ///
+    /// match priority &lt; 0:
+    ///    No match.
+    ///
+    /// match priority == 0:
+    ///    Match, but treat it as if matched an empty pattern.
+    ///
+    /// match priority &gt; 0:
+    ///    Match.
+    /// </summary>
+    public class LongestMatchRobotsMatchStrategy
+    {
+        public static int MatchAllow(string path, string pattern)
+        {
+            return Matches(path, pattern) ? pattern.Length : -1;
+        }
+
+        public static int MatchDisallow(string path, string pattern)
+        {
+            return Matches(path, pattern) ? pattern.Length : -1;
+        }
+
+        public static bool Matches(string path, string pattern)
+        {
+            var pathlen = path.Length;
+            var pos = new int[pathlen + 1];
+            int numpos = 1;
+            var patlen = pattern.Length;
+            for (var j = 0; j < patlen; j++)
+            {
+                var ch = pattern[j];
+                if (ch == '$' && j + 1 == patlen)
+                {
+                    return (pos[numpos - 1] == pathlen);
+                }
+
+                if (ch == '*')
+                {
+                    numpos = pathlen - pos[0] + 1;
+                    for (int i = 1; i < numpos; i++)
+                    {
+                        pos[i] = pos[i - 1] + 1;
+                    }
+                }
+                else
+                {
+                    // Includes '$' when not at end of pattern.
+                    int newnumpos = 0;
+                    for (int i = 0; i < numpos; i++)
+                    {
+                        if (pos[i] < pathlen && path[pos[i]] == ch)
+                        {
+                            pos[newnumpos++] = pos[i] + 1;
+                        }
+                    }
+
+                    numpos = newnumpos;
+                    if (numpos == 0) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TestRobotsTxt/Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using RobotsTxt;
+
+namespace TestRobotsTxt
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Theory]
+        [InlineData("/", "/", true)]
+        [InlineData("/", "/$", true)]
+        [InlineData("a", "b", false)]
+        [InlineData("abcd", "a", true)]
+        [InlineData("abcd", "a$", false)]
+        [InlineData("abcd", "a*", true)]
+        [InlineData("abcd", "a*b", true)]
+        [InlineData("abcd", "a*c", true)]
+        [InlineData("abcd", "a*d", true)]
+        [InlineData("abcd", "a*d$", true)]
+        [InlineData("abcd", "a*c$", false)]
+        [InlineData("/abcd/e//fg/hij/k/lm/nop/q/r/", "/*/*/*/*/*/*/*/*/*/*/*", true)]
+        public void TestMatch(string path, string pattern, bool expected)
+        {
+            var actual = LongestMatchRobotsMatchStrategy.Matches(path, pattern);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
