1f9bf9da020e3bc1bcdfa21c078fef94f3cd9b1f
Yves Bastide <stid@acm.org>
2023-09-26 14:25:13 +0200

    ♻️ More API changes
    * extract `PathAllowedByRobots`
    * make `MaybeEscapePattern` public
    
    Signed-off-by: Yves Bastide <stid@acm.org>

--- RobotsTxt/RobotsTxtParser.cs
+++ RobotsTxt/RobotsTxtParser.cs
@@ -120,7 +120,7 @@ namespace RobotsTxt
             }
         }
 
-        internal static ReadOnlySpan<byte> MaybeEscapePattern(ReadOnlySpan<byte> src)
+        public static ReadOnlySpan<byte> MaybeEscapePattern(ReadOnlySpan<byte> src)
         {
             int numToEscape = 0;
             bool needCapitalize = false;
